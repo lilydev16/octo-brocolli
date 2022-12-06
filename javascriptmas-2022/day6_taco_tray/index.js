c3eafbb5c1d51d4fc2501c3cbc2bb32665c8a428
lswatson16 <lauralynw16@gmail.com>
2022-12-05 23:41:55 -0500

    Create a function that generates a random number of tacos to display on UI

---
+++ javascriptmas-2022/day6_taco_tray/index.js
@@ -0,0 +1,24 @@
+// Challenge: Taco Tray
+// - Help our chef fill a tray with tacos! 
+
+/*
+Make this function return an array that contains 
+between one and ten taco emojis 🌮
+Use the following JavaScript concepts:
+    - Math.random()
+    - Math.floor()
+    - new Array()
+    - Array.fill()
+*/
+
+function getRandomNumberOfTacos() {
+  return new Array(Math.floor(Math.random() * 10 + 1)).fill('🌮')
+}
+
+function putTacosOnTray() {
+  return getRandomNumberOfTacos().map(function (taco) {
+      return `<div class="taco">${taco}</div>`
+  }).join('')
+}
+
+document.getElementById('tray').innerHTML = putTacosOnTray()
