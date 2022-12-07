e6dfbb75d2c3b897384a049c93a856af87c530ff
lswatson16 <lauralynw16@gmail.com>
2022-12-06 23:00:59 -0500

    Create a function that capitalizes the alternating character

---
+++ javascriptmas-2022/day7_alternating_caps/index.js
@@ -0,0 +1,17 @@
+/* Alternating Caps 
+ Write a function that takes in a string of letters
+ and returns a sentence in which every other letter is capitalized.
+
+Example input: "I'm so happy it's Monday"
+Example output: "I'M So hApPy iT'S MoNdAy"
+- Don't worry about punctuations
+*/
+
+function altCaps(str){
+  return str.split('')
+      .map((e, index) => index % 2 === 0 ? e.toUpperCase() : e.toLowerCase())
+      .join('')
+}
+
+console.log(altCaps("When you visit Portland you have to go to VooDoo Donuts"));
+console.log(altCaps("I'm so happy it's Monday"));
