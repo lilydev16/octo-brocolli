dd11c17f340856cad0c4fc9aea627feadafa29c6
lswatson16 <lauralynw16@gmail.com>
2022-12-13 20:40:47 -0500

    Create a function that sums of the values of the letters

---
+++ javascriptmas-2022/day14_count_vowel_consonant/main.js
@@ -0,0 +1,28 @@
+function countVowelConsonant(str) {
+  return str.split('').reduce((sum, char) => {
+      char.match(/[a, e, i, o, u]/g) ? sum += 1 : sum += 2
+      return sum
+  }, 0)
+  
+}
+
+
+
+/**
+* Test Suite 
+*/
+describe('countVowelConsonant()', () => {
+    it('returns total of vowels(1) and consonants(2) to be added', () => {
+        // arrange
+        const value = 'abcde';
+        
+        // act
+        const result = countVowelConsonant(value);
+
+        // log
+        console.log("result: ", result);
+        
+        // assert
+        expect(result).toBe(8);
+    });
+});
