e026433833dab807a7b36b6922e23b75e690086b
lswatson16 <lauralynw16@gmail.com>
2022-12-14 22:32:58 -0500

    Create a function that checks if the string is a palindrome

---
+++ javascriptmas-2022/day15_palindromes/index.js
@@ -0,0 +1,10 @@
+function isPalindrome(str){
+  return str === str.split('').reverse().join('') ? true : false
+}
+
+// Test your function
+console.log(isPalindrome("abba"));
+console.log(isPalindrome("civic"));
+console.log(isPalindrome("octopus"));
+console.log(isPalindrome("pumpkins"));
+console.log(isPalindrome("madam"));
