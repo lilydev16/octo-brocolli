11c5a7b91e033cf5a254ff76e3bbf069ba8e5326
lswatson16 <lauralynw16@gmail.com>
2022-12-28 16:14:03 -0500

    Display podcast list on UI; Add stylesheet

--- javascriptmas-2022/day20_find_free_podcasts/index.js
+++ javascriptmas-2022/day20_find_free_podcasts/index.js
@@ -1,16 +1,42 @@
 import podcasts from './data.js';
 
-//Solution A:
+// ----------- Query Selectors ------------
+const buttons = document.getElementById('buttons');
+const list = document.getElementById('list');
 
-// function getFreePodcasts(data) {
-//   return data
-//     .filter((podcast) => podcast.paid === false)
-//     .map((p) => {
-//       return { title: p.title, rating: p.rating, paid: p.paid };
-//     });
+// ------------- Event Listeners ------------
+buttons.addEventListener('click', (e) => {
+  // console.log(e.target)
+  // console.log(document.activeElement.id)
+  showPodcasts(e);
+});
+
+// -------------- DOM Updates ----------------
+function showPodcasts(e) {
+  const podcastList =
+    e.target.id === 'allPodcastBtn' ? podcasts : getFreePodcasts(podcasts);
+  clearList();
+  return podcastList.map(({ title, rating }) => {
+    list.innerHTML += `<div class="podcast-card"><p class="podcast-title">${title}</p><p>${rating} ⭐️</p></div>`;
+  });
+}
+
+function clearList() {
+  list.replaceChildren();
+}
+
+// --------------------------------------------
+//-------------- Solution A: --------------
+
+// function getFreePodcasts(data){
+//    return data
+//         .filter(podcast => podcast.paid === false)
+//         .map(p => {
+//             return {title: p.title, rating: p.rating, paid: p.paid}
+//         })
 // }
 
-// Refactored Solution A:
+//-------------- Refactored Solution A: --------------
 
 function getFreePodcasts(data) {
   return data
